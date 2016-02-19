da313635fb6c6c70f2863a8127b0a3cb1ec43f1a
Masha Vainblat <MashaVainblat@Mashas-MacBook-Pro.local>
2016-02-19 17:32:25 -0500

    found hex model

--- server.js
+++ server.js
@@ -4,13 +4,20 @@ console.log("Serving");
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
-var logger = require("morgan");
+// var logger = require("morgan");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var mongoose = require("mongoose");
 
-//MIDDLEWARE
+//CONNECT TO DB:
+mongoose.connect("mongodb://localhost/hex_color_palette")
 
+//MIDDLEWARE
+// app.use(logger("dev"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 //CONTROLLERS
 
