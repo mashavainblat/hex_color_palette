7f93b8522ee0c790bb157e67b2ea9975d537e673
Masha Vainblat <MashaVainblat@Mashas-MacBook-Pro.local>
2016-02-19 14:51:51 -0500

    server running

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 console.log("Serving");
+
+//REQUIREMENTS
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var mongoose = require("mongoose");
+
+//MIDDLEWARE
+
+
+//CONTROLLERS
+
+//LISTENER
+app.listen(port, function(){
+	console.log("==========================");
+	console.log("Listening on port: " + port);
+	console.log("==========================");
+});
