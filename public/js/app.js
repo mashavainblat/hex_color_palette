42a527fcba78c4f187e1ad59c5f5360f61ec9641
Masha Vainblat <MashaVainblat@Mashas-MacBook-Pro.local>
2016-02-22 19:22:38 -0500

    input value of colors matches clicked div

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,31 @@
-// $(document).ready(function(){
+ $(document).ready(function(){
 	console.log("Hex Color Palette");
 
 	// var hexColor = document.getElementById("hexColor");
 	// console.log(hexColor);
 
-	function addToPalette(color){
-			console.log("you clicked");
-			console.log(color)
-			console.log(color.innerHTML)
-			var hexCode = document.getElementById("p");
-			console.log(hexCode)
+	$('.hexColor').click(function(){ 
+		console.log("you clicked " + $(this).attr('id'));
+		var hexCode = $(this).attr('id');
+		var createNewPalette = document.getElementById("createNewPalette");
+		console.log(createNewPalette);
+
+		var input = document.createElement("input");
+		input.type="color";
+		input.name="palette";
+		input.value=hexCode;
+		createNewPalette.appendChild(input);
+	}); 
+
+
+
+	
+			
+			//console.log(color)
+			// console.log(color.innerHTML)
+			// console.log(color.colors)
+			// var hexCode = document.getElementById("p");
+			// console.log(hexCode)
 			//get createNewPalette div to append clicked colors to form
-			var createNewPalette = document.getElementById("createNewPalette");
-			console.log(createNewPalette);
 
-			//create input to push info into database
-			var input = document.createElement("input");
-			input.type="color";
-			input.name="palette";
-			input.value="#cccccc";
-			createNewPalette.appendChild(input);
-
-	}
-
-// }); //ends document.ready
+}); //ends document.ready
